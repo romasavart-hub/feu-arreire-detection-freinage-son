125da37591a7060d591fbe74f39bb7e2f4b44492
romasavart-hub <roma_savart@yahoo.fr>
2026-03-11 15:40:37 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Ouvrir cette page à [https://romasavart-hub.github.io/feu-arreire-detection-freinage-son/](https://romasavart-hub.github.io/feu-arreire-detection-freinage-son/)
+
+## Utiliser comme extension
+
+Ce dépôt peut être ajouté en tant qu'**extension** dans MakeCode.
+
+* ouvrir [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* cliquez sur **Nouveau projet**
+* cliquez sur **Extensions** dans le menu engrenage
+* recherchez **https://github.com/romasavart-hub/feu-arreire-detection-freinage-son** et importez
+
+## Éditer ce projet
+
+Éditer ce dépôt dans MakeCode.
+
+* ouvrir [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* cliquez sur **Importer** puis cliquez sur **Importer l'URL **
+* collez **https://github.com/romasavart-hub/feu-arreire-detection-freinage-son** et cliquez sur importer
+
+#### Métadonnées (utilisées pour la recherche, le rendu)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,82 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 10; index++) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 10; index++) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+})
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # . # . #
+    . . # . .
+    . . # . .
+    `)
